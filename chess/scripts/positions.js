def3c88841381c123e8d4203d9a594f363d3aa2a
Sergey <ermolov_sergey@inbox.ru>
2022-09-17 10:55:58 +0300

    Урок 5
    Выполнил здание 5-того урока

---
+++ chess/scripts/positions.js
@@ -0,0 +1,104 @@
+let positions = [
+    //названия и координаты белых фигур, кроме пешек
+    {
+        coordCol: 'A',
+        coordRow: 1,
+        figure: 'rook',
+        color: 'white'
+    },
+    {
+        coordCol: 'B',
+        coordRow: 1,
+        figure: 'knight',
+        color: 'white'
+    },
+    {
+        coordCol: 'C',
+        coordRow: 1,
+        figure: 'bishop',
+        color: 'white'
+    },
+    {
+        coordCol: 'D',
+        coordRow: 1,
+        figure: 'queen',
+        color: 'white'
+    },
+    {
+        coordCol: 'E',
+        coordRow: 1,
+        figure: 'king',
+        color: 'white'
+    },
+    {
+        coordCol: 'F',
+        coordRow: 1,
+        figure: 'bishop',
+        color: 'white'
+    },
+    {
+        coordCol: 'G',
+        coordRow: 1,
+        figure: 'knight',
+        color: 'white'
+    },
+    {
+        coordCol: 'H',
+        coordRow: 1,
+        figure: 'rook',
+        color: 'white'
+    },
+
+
+
+
+    //названия и координаты черных фигур, кроме пешек
+    {
+        coordCol: 'A',
+        coordRow: 8,
+        figure: 'rook',
+        color: 'black'
+    },
+    {
+        coordCol: 'B',
+        coordRow: 8,
+        figure: 'knight',
+        color: 'black'
+    },
+    {
+        coordCol: 'C',
+        coordRow: 8,
+        figure: 'bishop',
+        color: 'black'
+    },
+    {
+        coordCol: 'D',
+        coordRow: 8,
+        figure: 'queen',
+        color: 'black'
+    },
+    {
+        coordCol: 'E',
+        coordRow: 8,
+        figure: 'king',
+        color: 'black'
+    },
+    {
+        coordCol: 'F',
+        coordRow: 8,
+        figure: 'bishop',
+        color: 'black'
+    },
+    {
+        coordCol: 'G',
+        coordRow: 8,
+        figure: 'knight',
+        color: 'black'
+    },
+    {
+        coordCol: 'H',
+        coordRow: 8,
+        figure: 'rook',
+        color: 'black'
+    },
+];
